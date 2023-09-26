f8b2ec78e12ca947eaa585d4a7b8414b48789610
Ricco Chong <riccojchong@gmail.com>
2023-09-26 11:13:20 -0700

    made some changes to chatpox and wrote some notes

---
+++ Notes/Modules.md
@@ -0,0 +1,26 @@
+**Modules** are pieces of code that we can borrow in Python
+This includes things like **functions** 
+
+>For example, in random we use the .choice() function
+>to choose something randomly from a [[lists]]
+
+These pieces of code are *not* automatically included,
+so we need to `import` them into our code explicitly
+
+
+# `import`
+The `import` keyword loads the module into our Python file
+**`imports` should be at the top of our file, under the header**
+
+# `The time module`
+Time allows us to play around with anything related to time
+Specifically, we can use `time` to pause our code
+
+
+```python
+import time
+
+# Pause our code for 1 second
+time.sleep(1)
+```
+
--- chatbot.py
+++ chatbot.py
@@ -2,3 +2,38 @@
  # Auther : Ricco Chong
  # Date : Sep 21 2023
 
+import time
+import random
+
+# Greet the user
+#pause in between lines of  dialogue
+print("Hello there! ")
+time.sleep(1)
+print("i'm a crude chatbot, heres to talk to you. ")
+time.sleep(1.5)
+
+# Get the user's neame and store in a variable
+user_name = input ("So... what's your name? ")
+time.sleep(1)
+
+# Respond with the user's name
+print (f"it's nice to meet you, {user_name}! ")
+time.sleep(2)
+# Ask the user what their favourite food is
+fave_food = input ("whats your favorite food? ")
+
+# Respond with something appropriate(Respond with something that is not TOO repetitive)
+# Create a list of appropriate responses
+time.sleep(1)
+list_of_fave_food_responses = [
+    "Mmmmm. That sounds delicious.",
+    f"Yes, {fave_food} is one of my favorites too!",
+    "cool.",
+    "intresting, i've never tried that before!"
+]
+
+# Choose one response randomly from the list 
+randome_response = random.choice (list_of_fave_food_responses)
+
+# Print out the chosen response
+print(randome_response)
