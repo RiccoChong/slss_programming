6f38556c7025638d54ce9a1b80f51245f6e6aa82
Ricco Chong <riccojchong@gmail.com>
2024-01-22 10:54:20 -0800

    h

--- spotify_analyzer.py
+++ spotify_analyzer.py
@@ -3,36 +3,53 @@
 # 16 janurary 2023 
 
 # Version 0.1
-#   - from the data set, get all the songs performed by Drake
+#   - From the data set, get all the songs
+#     performed my Drake
 
 import csv
 
-def find_all_songs(artist: str) -> list:
-    """Returns a list of all songs from a given artist
-"""
+# Open up the file
+with open("./spotify.csv") as f:
+    # ---- Look for all songs performed by Drake
+    #      Use linear search
+    # Create a csv reader object
+    csv_reader = csv.DictReader(f)
 
-    # Open up the file
-    with open ("./spotify.csv") as f:
-        # --- Look for all songs performed by Drake using linear search---
-        # create a csv reader object
-        csv_reader = csv.DictReader(f)
+    # Create a list to store all Drake's songs
+    drake_songs = []
 
-        # Create a list to store all Drake's songs
-        drake_song = []
+    # for every song in the .csv file
+    for line in csv_reader:
+        if "drake" in line["artist"].lower():
+            # add it to the Drake's songs list
+            drake_songs.append(
+                (line["artist"], line["song_title"], line["danceability"])
+            )
 
-        # Create a counter for the current line number
-        line_num = 0
-
-        # for every song in the .csv file
-        for line in csv_reader:
-            # If this song is sung by Drake
-            if "drake" in line["artist"].lower():
-                # Add it to the song list
-                song.append(line["artist"], line["song_title"], line["danceability"])
-    
-    return song
-
-
-for song in drake_song:
+# Print out all songs that have
+# a danceability of >= 0.6
+for song in drake_songs:
     if float(song[-1]) >= 0.6:
         print(song)
+
+
+# --- Sorting Algorithm
+# --- Selection Sort
+
+# Starting at the begnning of the list moving to the end
+for i in range(len(drake_songs)):
+    # Set the current to the smallest value
+    smallest_danceability = drake_songs[i][-1]
+    smallest_index = i
+
+    # For the rest of the list
+    for j in range(i + 1, len(drake_songs)):
+        # Check to see if this is smaller
+        if drake_songs[j][-1] < smallest_danceability:
+            smallest_danceability = drake_songs[j][-1]
+            smallest_index = j
+
+# Swap the current position with the smallest number we found
+drake_songs[i], drake_songs[smallest_index] = drake_songs[smallest_index], drake_songs[i]
+
+print("--- Sorted Drake")
